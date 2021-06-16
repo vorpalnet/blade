e6d849c55160a0bde1127565166f94015a8040ec
Jeff McDonald <jeff@mcdonald.net>
2021-06-16 17:23:49 -0500

    Added MIT license;

--- vorpal-blade-services-options/src/org/vorpal/blade/services/options/OptionsSettings.java
+++ vorpal-blade-services-options/src/org/vorpal/blade/services/options/OptionsSettings.java
@@ -1,8 +1,32 @@
+/**
+ *  MIT License
+ *  
+ *  Copyright (c) 2021 Vorpal Networks, LLC
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 package org.vorpal.blade.services.options;
 
 import java.io.Serializable;
 
-public class OptionsSettings implements Serializable{
+public class OptionsSettings implements Serializable {
 
 	String accept = "application/sdp";
 	String acceptLanguage = "en";
@@ -11,11 +35,6 @@ public class OptionsSettings implements Serializable{
 	String userAgent = "OCCAS 7.1";
 	String allowEvents = "talk, hold";
 
-	
-	
-	
-	
-	
 	public String getAllow() {
 		return allow;
 	}
