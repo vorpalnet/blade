2316c360d053dff2ff500cdf61a7409067b44724
Jeff McDonald <jeff@mcdonald.net>
2021-08-18 08:00:50 -0500

    Initial commit.

---
+++ vorpal-blade-shared-libraries/src/main/java/org/vorpal/blade/framework/b2bua/Cancel.java
@@ -0,0 +1,70 @@
+/**
+ *  MIT License
+ *  
+ *  Copyright (c) 2021 Vorpal Networks, LLC
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package org.vorpal.blade.framework.b2bua;
+
+import java.util.Collection;
+
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipSession;
+import javax.servlet.sip.SipSession.State;
+import javax.servlet.sip.UAMode;
+
+import org.vorpal.blade.framework.callflow.Callflow;
+
+public class Cancel extends Callflow {
+	private static final long serialVersionUID = 1L;
+	private SipServletRequest aliceRequest;
+	private B2buaListener b2buaListener;
+
+	public Cancel(B2buaListener b2buaListener) {
+		this.b2buaListener = b2buaListener;
+	}
+
+	@Override
+	public void process(SipServletRequest request) throws Exception {
+		aliceRequest = request;
+		SipSession linkedSession = getLinkedSession(aliceRequest.getSession());
+
+		Collection<SipServletRequest> requests = linkedSession.getActiveRequests(UAMode.UAC);
+		for (SipServletRequest rq : requests) {
+			if (rq.getSession().getState().equals(State.EARLY)) {
+				SipServletRequest cancel = copyContentAndHeaders(request, rq.createCancel());
+				b2buaListener.callAbandoned(cancel);
+				sendRequest(cancel);
+			}
+		}
+	}
+
+//	private void callEvents(SipServletMessage alice, SipServletMessage bob) throws Exception {
+//		if (((String) bob.getSession().getAttribute("USER_TYPE")).equals("CALLEE")) {
+//			this.sipServlet.calleeEvent(bob);
+//			this.sipServlet.callerEvent(alice);
+//		} else {
+//			this.sipServlet.calleeEvent(alice);
+//			this.sipServlet.callerEvent(bob);
+//		}
+//	}
+
+}
