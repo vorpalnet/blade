4f9406862b10b0d849e0954803f9ac0e831187b7
Jeff McDonald <jeff@mcdonald.net>
2021-08-18 08:00:50 -0500

    Initial commit.

---
+++ vorpal-blade-shared-libraries/src/main/java/org/vorpal/blade/framework/b2bua/B2buaListener.java
@@ -0,0 +1,50 @@
+/**
+ *  MIT License
+ *  
+ *  Copyright (c) 2021 Vorpal Networks, LLC
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package org.vorpal.blade.framework.b2bua;
+
+import java.io.Serializable;
+
+import javax.servlet.sip.SipServletContextEvent;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+
+public interface B2buaListener extends Serializable {
+	public void b2buaCreated(SipServletContextEvent event) throws Exception;
+
+	public void b2buaDestroyed(SipServletContextEvent event) throws Exception;
+
+	public void callStarted(SipServletRequest request) throws Exception;
+
+	public void callAnswered(SipServletResponse response) throws Exception;
+
+	public void callConnected(SipServletRequest request) throws Exception;
+
+	public void callCompleted(SipServletRequest request) throws Exception;
+
+	public void callDeclined(SipServletResponse response) throws Exception;
+
+	public void callAbandoned(SipServletRequest request) throws Exception;
+
+}
---
+++ vorpal-blade-shared-libraries/src/main/java/org/vorpal/blade/framework/b2bua/Passthru.java
@@ -0,0 +1,74 @@
+/**
+ *  MIT License
+ *  
+ *  Copyright (c) 2021 Vorpal Networks, LLC
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package org.vorpal.blade.framework.b2bua;
+
+import javax.servlet.sip.SipServletMessage;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+import javax.servlet.sip.SipSession;
+
+import org.vorpal.blade.framework.callflow.Callflow;
+
+public class Passthru extends Callflow {
+	private static final long serialVersionUID = 1L;
+	private SipServletRequest aliceRequest;
+	private B2buaListener b2buaListener;
+
+	public Passthru(B2buaListener b2buaListener) {
+		this.b2buaListener = b2buaListener;
+	}
+
+	@Override
+	public void process(SipServletRequest request) throws Exception {
+		SipServletRequest bobRequest;
+
+		aliceRequest = request;
+		SipSession sipSession = getLinkedSession(aliceRequest.getSession());
+
+		bobRequest = sipSession.createRequest(request.getMethod());
+		copyContentAndHeaders(aliceRequest, bobRequest);
+
+		callEvents(aliceRequest, bobRequest);
+
+		sendRequest(bobRequest, (bobResponse) -> {
+			SipServletResponse aliceResponse = aliceRequest.createResponse(bobResponse.getStatus());
+			copyContentAndHeaders(bobResponse, aliceResponse);
+			callEvents(aliceResponse, bobResponse);
+			sendResponse(aliceResponse);
+		});
+
+	}
+
+	private void callEvents(SipServletMessage alice, SipServletMessage bob) throws Exception {
+//		if (((String) bob.getSession().getAttribute("USER_TYPE")).equals("CALLEE")) {
+//			this.sipServlet.calleeEvent(bob);
+//			this.sipServlet.callerEvent(alice);
+//		} else {
+//			this.sipServlet.calleeEvent(alice);
+//			this.sipServlet.callerEvent(bob);
+//		}
+	}
+
+}
