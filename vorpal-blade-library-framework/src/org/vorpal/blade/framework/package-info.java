d1c7d504f048bec7fcb754bc98becaa15b3fb29f
Jeff McDonald <jeff@mcdonald.net>
2021-12-29 17:35:08 -0600

    Adding Javadocs;

---
+++ vorpal-blade-library-framework/src/org/vorpal/blade/framework/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This is the base package of the Vorpal:BLADE framework library. The purpose
+ * of this library is to provide the developer a convenient and intuitive
+ * approach to developing SIP Servlet applications.
+ *
+ * @since 1.0
+ * @see java.awt
+ */
+package org.vorpal.blade.framework;
