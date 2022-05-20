e5f2dd192bc6b0e4a9db71e1c252b9e85d4c435d
Jeff McDonald <jeff@mcdonald.net>
2022-05-20 09:11:33 -0500

    Updated docs.

--- vorpal-blade-library-framework/src/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Welcome to the Vorpal:BLADE (Blended Layer Application Development Environment),
- * the purpose of which is to create a simple and easy to use framework for blending
- * SIP and HTML applications beyond what is provided in the standard SIP Servlet APIs.
- *
- * @since 2.0
- */
