420f7a006b3022221dcf686240aed88e5fc84d70
jeff <jeff@localhost>
2024-02-20 00:39:09 -0600

    More CRUD problems.

--- vorpal-blade-services-crud/src/main/java/org/vorpal/blade/services/crud/config/BladeDelete.java
+++
@@ -1,6 +0,0 @@
-package org.vorpal.blade.services.crud.config;
-
-public class BladeDelete extends BladeRule{
-
-	
-}
--- vorpal-blade-services-crud/src/main/java/org/vorpal/blade/services/crud/config/BladeRuleSet.java
+++
@@ -1,8 +0,0 @@
-package org.vorpal.blade.services.crud.config;
-
-import java.util.LinkedList;
-import java.util.List;
-
-public class BladeRuleSet {
-	public List<BladeRule> rules = new LinkedList<>();
-}
