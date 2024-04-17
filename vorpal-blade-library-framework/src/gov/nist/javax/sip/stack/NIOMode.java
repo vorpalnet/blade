bf5bef572d272913df4d15198faf3886e66363f2
jeff <jeff@gamera>
2024-04-17 06:08:08 -0500

    Removed unnecessary gov.nist classes.

--- vorpal-blade-library-framework/src/gov/nist/javax/sip/stack/NIOMode.java
+++
@@ -1,5 +0,0 @@
-package gov.nist.javax.sip.stack;
-
-public enum NIOMode {
-    BLOCKING,NONBLOCKING
-}
--- vorpal-blade-library-framework/src/org/vorpal/blade/library/shared/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This project is simply to bundle the "vorpal-blade-library-framework" into a
- * WAR file. There are no source files.
- */
-package org.vorpal.blade.library.shared;
