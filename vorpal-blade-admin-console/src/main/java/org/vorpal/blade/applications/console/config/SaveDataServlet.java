25d4d3255f8e09b5d58c5648f15d1a62c8666b73
jeff <jeff@vorpal.net>
2024-07-26 07:45:24 -0500

    Forgot it call .initialize() on SettingsManager

---
+++ vorpal-blade-admin-console/src/main/java/org/vorpal/blade/applications/console/config/SaveDataServlet.java
@@ -0,0 +1,46 @@
+package org.vorpal.blade.applications.console.config;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.vorpal.blade.applications.console.config.test.ConfigHelper;
+
+@WebServlet("/saveData")
+public class SaveDataServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");// setting the content type
+
+		String app = req.getParameter("app");
+		String configType = req.getParameter("configType");
+		String data = req.getParameter("data");
+
+		System.out.println("app = " + app);
+		System.out.println("configType = " + configType);
+		System.out.println("data = " + data);
+
+		ConfigHelper configHelper = new ConfigHelper(app);
+
+		switch (configType) {
+		case "domain":
+			configHelper.saveDomainJson(data);
+			break;
+		case "cluster":
+			configHelper.saveClusterJson(data);
+			break;
+		case "server":
+			configHelper.saveServerJson(data);
+		}
+
+		PrintWriter pw = res.getWriter();// get the stream to write the data
+		pw.println("<html><body>");
+		pw.println("</body></html>");
+		pw.close();// closing the stream
+	}
+}
