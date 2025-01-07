71bcba41430e9dc24753078328a850ec76cdf3ce
jeff <jeff@vorpal.net>
2025-01-07 13:08:11 -0600

    Options Serializable problem.

--- vorpal-blade-services-options/src/org/vorpal/blade/services/options/OptionsSettingsSample.java
+++ vorpal-blade-services-options/src/org/vorpal/blade/services/options/OptionsSettingsSample.java
@@ -26,64 +26,16 @@ package org.vorpal.blade.services.options;
 
 import java.io.Serializable;
 
-import org.vorpal.blade.framework.v2.config.Configuration;
+import org.vorpal.blade.framework.v2.config.SessionParametersDefault;
+import org.vorpal.blade.framework.v2.logging.LogParametersDefault;
 
-public class OptionsSettings extends Configuration implements Serializable {
+public class OptionsSettingsSample extends OptionsSettings implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private String accept = "application/sdp";
-	private String acceptLanguage = "en";
-	private String allow = "INVITE, ACK, BYE, CANCEL, REGISTER, OPTIONS, PRACK, SUBSCRIBE, NOTIFY, PUBLISH, INFO, REFER, MESSAGE, UPDATE";
-	private String supported = "replaces";
-	private String userAgent = "OCCAS";
-	private String allowEvents = "talk, hold";
 
-	public String getAllow() {
-		return allow;
-	}
-
-	public void setAllow(String allow) {
-		this.allow = allow;
-	}
-
-	public String getAccept() {
-		return accept;
-	}
-
-	public void setAccept(String accept) {
-		this.accept = accept;
-	}
-
-	public String getAcceptLanguage() {
-		return acceptLanguage;
-	}
-
-	public void setAcceptLanguage(String acceptLanguage) {
-		this.acceptLanguage = acceptLanguage;
-	}
-
-	public String getSupported() {
-		return supported;
-	}
-
-	public void setSupported(String supported) {
-		this.supported = supported;
-	}
-
-	public String getUserAgent() {
-		return userAgent;
-	}
-
-	public void setUserAgent(String userAgent) {
-		this.userAgent = userAgent;
-	}
-
-	public String getAllowEvents() {
-		return allowEvents;
-	}
-
-	public void setAllowEvents(String allowEvents) {
-		this.allowEvents = allowEvents;
+	public OptionsSettingsSample() {
+		this.logging = new LogParametersDefault();
+		this.session = new SessionParametersDefault();
 	}
 
 }
