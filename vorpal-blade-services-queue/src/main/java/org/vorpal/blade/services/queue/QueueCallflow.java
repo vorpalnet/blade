02faa426934979c95d36c03b14288f90395a56a6
jeff <jeff@vorpal.net>
2025-01-13 19:30:03 -0800

    Queue is acting wonky.

--- vorpal-blade-services-queue/src/main/java/org/vorpal/blade/services/queue/QueueCallflow.java
+++ vorpal-blade-services-queue/src/main/java/org/vorpal/blade/services/queue/QueueCallflow.java
@@ -152,8 +152,10 @@ public class QueueCallflow extends Callflow {
 
 //			sipLogger.finer(appSession, "is the app session null? " + ((appSession == null) ? true : false));
 
-			if (appSession != null && appSession.isValid() && //
-					aliceRequest.getSession() != null && aliceRequest.getSession().isValid()) {
+//			if (appSession != null && appSession.isValid() && //
+//					aliceRequest.getSession() != null && aliceRequest.getSession().isValid()) {
+
+			if (appSession != null) {
 
 //				sipLogger.finer(appSession, "is the queue state canceled? " + this.stateEquals(QueueState.CANCELED));
 //				sipLogger.finer(appSession, "is the app session valid? " + appSession.isValid());
