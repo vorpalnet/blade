6d023bbd752254cf6408d621708f5b3f5b1d4238
jeff <jeff@vorpal.net>
2025-02-20 05:56:21 -0600

    Updated processing of origin or destination session attributes as defined in the configuraiton file.

--- vorpal-blade-library-framework/src/org/vorpal/blade/framework/v2/config/SessionParameters.java
+++ vorpal-blade-library-framework/src/org/vorpal/blade/framework/v2/config/SessionParameters.java
@@ -10,35 +10,14 @@ import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 public class SessionParameters implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public enum DialogType {
-		origin, destination
-	};
-
 	@JsonPropertyDescription("Set Application Session expiration in minutes.")
 	public Integer expiration = null;
 
 	@JsonPropertyDescription("Set Keep-Alive parameters.")
 	public KeepAliveParameters keepAlive = null;
 
-	@JsonPropertyDescription("Apply attributes to either origin or destination dialog. Default: origin")
-	public DialogType dialog;
-
 	@JsonPropertyDescription("List of selectors for creating session (SipApplicationSession) lookup keys.")
 	public List<AttributeSelector> sessionSelectors = null;
-
-	public DialogType getDialog() {
-
-		if (dialog == null) {
-			return DialogType.origin;
-		} else {
-			return dialog;
-		}
-	}
-
-	public SessionParameters setDialog(DialogType dialog) {
-		this.dialog = dialog;
-		return this;
-	}
 
 	public List<AttributeSelector> getSessionSelectors() {
 		return sessionSelectors;
