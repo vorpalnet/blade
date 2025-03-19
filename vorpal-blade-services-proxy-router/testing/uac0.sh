1c51602e3b53783c8737fd0a6cf76c3f45950b4b
jeff <jeff@vorpal.net>
2025-03-19 11:59:27 -0400

    Updated proxy-router

---
+++ vorpal-blade-services-proxy-router/testing/uac0.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SERVICE=18165551234
+export DURATION=5000
+export OCCAS_HOST=occas
+export OCCAS_PORT=5099
+export UAS_HOST=occas
+export UAS_PORT=5099
+
+sipp -d ${DURATION} -rsa ${OCCAS_HOST}:${OCCAS_PORT} -m 1 -s ${SERVICE} -sf uac.xml ${UAS_HOST}:${UAS_PORT}
