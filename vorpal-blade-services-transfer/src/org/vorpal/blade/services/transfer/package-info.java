6ff2007152d3686db6abfce5f121ba5f1993b071
jeff <jeff@vorpal.net>
2025-03-25 08:08:48 -0500

    Added 'userAgent' designation of either 'caller' or 'callee' to transfer dialog.

---
+++ vorpal-blade-services-transfer/src/org/vorpal/blade/services/transfer/package-info.java
@@ -0,0 +1,11 @@
+package org.vorpal.blade.services.transfer;
+
+/**
+* Application: Transfer
+* 
+* This application implements a 'blind' transfer when it receives a REFER message.
+* 
+* <img src="org/vorpal/blade/services/api/callflows/doc-files/blind_transfer_success.png">
+* 
+* It also has a REST API.
+*/
