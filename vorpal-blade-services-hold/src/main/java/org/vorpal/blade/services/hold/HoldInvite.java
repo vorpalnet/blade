93f47dc02a472eb847bbeb146da4a80aa3b67394
jeff <jeff@vorpal.net>
2025-05-07 12:32:14 -0500

    Hold update.

--- vorpal-blade-services-hold/src/main/java/org/vorpal/blade/services/hold/HoldInvite.java
+++ vorpal-blade-services-hold/src/main/java/org/vorpal/blade/services/hold/HoldInvite.java
@@ -14,10 +14,21 @@ public class HoldInvite extends Callflow {
 
 	@Override
 	public void process(SipServletRequest request) throws ServletException, IOException {
-
+		String body;
 		SipServletResponse response = request.createResponse(200);
-		response.setHeader("Allow", "INVITE, ACK, BYE");
-		response.setContent(blackhole, "application/sdp");
+
+		Object obj = request.getContent();
+		if (obj instanceof String) {
+			body = new String((String) obj);
+		} else {
+			body = new String((byte[]) obj);
+		}
+
+		body = body.replace("a=sendrecv", "a=inactive");
+
+		response.setHeader("Allow", request.getHeader("Allow"));
+
+		response.setContent(body, request.getContentType());
 
 		sendResponse(response, (ack) -> {
 			// do nothing;
@@ -25,14 +36,14 @@ public class HoldInvite extends Callflow {
 
 	}
 
-	static final String blackhole = "" //
-			+ "v=0\r\n" //
-			+ "o=- 15474517 1 IN IP4 127.0.0.1\r\n" //
-			+ "s=cpc_med\r\n" //
-			+ "c=IN IP4 0.0.0.0\r\n" //
-			+ "t=0 0\r\n" //
-			+ "m=audio 23348 RTP/AVP 0\r\n" //
-			+ "a=rtpmap:0 pcmu/8000\r\n" //
-			+ "a=inactive\r\n";
+//	static final String blackhole = "" //
+//			+ "v=0\r\n" //
+//			+ "o=- 15474517 1 IN IP4 127.0.0.1\r\n" //
+//			+ "s=cpc_med\r\n" //
+//			+ "c=IN IP4 0.0.0.0\r\n" //
+//			+ "t=0 0\r\n" //
+//			+ "m=audio 23348 RTP/AVP 0\r\n" //
+//			+ "a=rtpmap:0 pcmu/8000\r\n" //
+//			+ "a=inactive\r\n";
 
 }
