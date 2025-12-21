55ba1b13094dcebb481951c2f5443e5d2039a122
jeff <jeff@vorpal.net>
2025-12-21 16:07:35 -0600

    Addd WebSocketFileManager.

--- vorpal-blade-admin-configurator/src/main/java/org/vorpal/blade/applications/console/config/WebSocketFileManager.java
+++ vorpal-blade-admin-configurator/src/main/java/org/vorpal/blade/applications/console/config/WebSocketFileManager.java
@@ -6,4 +6,6 @@ import javax.servlet.http.HttpServlet;
 @WebServlet("/filemanager/*")
 public class WebSocketFileManager extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
 }
