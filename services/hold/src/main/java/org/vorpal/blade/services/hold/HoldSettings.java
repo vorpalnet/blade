6f9ea062b1cda43385b19ff5aa421553e9739d4f
jeff <jeff@vorpal.net>
2026-05-05 09:49:46 -0400

    Hold Settings

--- services/hold/src/main/java/org/vorpal/blade/services/hold/HoldSettings.java
+++ services/hold/src/main/java/org/vorpal/blade/services/hold/HoldSettings.java
@@ -2,12 +2,12 @@ package org.vorpal.blade.services.hold;
 
 import java.io.Serializable;
 
-import org.vorpal.blade.framework.v2.config.RouterConfig;
+import org.vorpal.blade.framework.v2.config.Configuration;
 
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle;
 
 @JsonSchemaTitle(value = "Hold")
-public class HoldSettings extends RouterConfig implements Serializable {
+public class HoldSettings extends Configuration implements Serializable {
 	private static final long serialVersionUID = 2L;
 
 }
--- services/hold/src/main/java/org/vorpal/blade/services/hold/HoldSettingsSample.java
+++ services/hold/src/main/java/org/vorpal/blade/services/hold/HoldSettingsSample.java
@@ -1,7 +1,15 @@
 package org.vorpal.blade.services.hold;
 
+import org.vorpal.blade.framework.v2.config.SessionParametersDefault;
+import org.vorpal.blade.framework.v2.logging.LogParametersDefault;
+
 public class HoldSettingsSample extends HoldSettings {
 
 	private static final long serialVersionUID = 1L;
 
+	public HoldSettingsSample() {
+
+		this.logging = new LogParametersDefault();
+		this.session = new SessionParametersDefault();
+	}
 }
